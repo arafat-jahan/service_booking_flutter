1efd9e2335de0ae152b517be9f1c031aedddc13d
arafat-jahan <ajh.official15@gmail.com>
2026-01-22 18:48:40 +0600

    feat: add HomePage with service cards

--- lib/pages/service_details_page.dart
+++ lib/pages/service_details_page.dart
@@ -1,80 +1,72 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
+import 'package:service_app/pages/booking_page.dart';
+
 import '../models/service_model.dart';
-import 'booking_page.dart';
+import 'package:flutter/material.dart';
+
+
 
 class ServiceDetailsPage extends StatelessWidget {
-  final Service service;
 
-  const ServiceDetailsPage({super.key, required this.service});
+  final Service service;
+  const ServiceDetailsPage({super.key, required this. service});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(service.name),
+      appBar: AppBar(title: Text(service.name),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // IMAGE
+          //image
+
           Image.network(
             service.image,
             height: 220,
             width: double.infinity,
             fit: BoxFit.cover,
           ),
-
-          // ICON + NAME
-          Padding(
-            padding: const EdgeInsets.all(16),
+          
+          
+          //icon+name
+          
+          Padding(padding: const EdgeInsets.all(16),
             child: Row(
               children: [
-                Icon(service.icon, size: 35),
-                const SizedBox(width: 12),
-                Text(
-                  service.name,
-                  style: const TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+                Icon(service.icon,size:35),
+                const SizedBox(width: 12,),
+                Text(service.name, style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),)
               ],
             ),
+          
           ),
-
-          // DESCRIPTION
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
+          //Description
+          const Padding(padding: EdgeInsets.symmetric(horizontal: 16),
             child: Text(
               "Professional service at your doorstep. Book now to get fast, reliable service from trained professionals.",
               style: TextStyle(fontSize: 16),
+
             ),
           ),
 
-          const SizedBox(height: 20),
+          const SizedBox(height: 20,),
 
-          // BOOK NOW BUTTON
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
+          ///book now button
+
+          Padding(padding: const EdgeInsets.symmetric(horizontal: 16),
             child: SizedBox(
               width: double.infinity,
               height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BookingPage(service: service),
-                    ),
-                  );
-                },
-                child: const Text(
-                  "Book Now",
-                  style: TextStyle(fontSize: 18),
+              child: ElevatedButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> BookingPage(service: service),
                 ),
-              ),
+                );
+              }, child: const Text("Book Now",style: TextStyle(fontSize: 18),)),
             ),
-          ),
+          )
+
+
         ],
       ),
     );
